e4ee1333e5150a8f158876114fbcc12d7df7490d
고현빈 <khb4130@naver.com>
2024-12-20 21:04:16 +0900

    #1 Spring boot init

---
+++ pj-lms-back/src/main/java/com/pro/lms/PjLmsBackApplication.java
@@ -0,0 +1,13 @@
+package com.pro.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjLmsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjLmsBackApplication.class, args);
+	}
+
+}
